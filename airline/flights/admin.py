aebbffd54ba3d71f3ef488c6a1d60d8255b377d0
eduper1 <eduper.havard@gmail.com>
2021-07-07 08:08:46 +0100

    manuplating admin page manually

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
 admin.site.register(Airports)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passenger)
